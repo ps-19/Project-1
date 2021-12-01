75387aec9f24e1da8564e308d1c0f16b65e96622
ps-19 <priyansh.singh100@gmail.com>
2021-12-02 00:08:22 +0530

    Second Commit

---
+++ src/Components/Display.js
@@ -0,0 +1,157 @@
+import React from "react";
+import "./Display.css";
+
+
+function Display(props) {
+  
+  const data = props.data;
+  const data2 = props.data2;
+
+  // console.log(data);
+  // console.log(data2);
+    
+  const iconurl =
+    "http://openweathermap.org/img/wn/" +
+    `${data.cod !== 404 ? data.weather[0].icon : null}` +
+    ".png";
+
+
+  return (
+    <div className="displayweather">
+      {data.cod !== 404 ? (
+        <React.Fragment>
+          <div className="maincard">
+            <span className="cardtitle">
+              {data.name} , {data.sys.country}. Weather
+            </span>
+            <span className="cardsubtitle">
+              As of {new Date().toLocaleTimeString()}
+            </span>
+
+
+            <h1>
+              {" "}
+              {Math.floor(data.main.temp - 273.15)}
+              <sup>o</sup>
+            </h1>
+
+            <span className="weather-main">{data.weather[0].main}</span>
+            <img className="weather-icon" src={iconurl} alt="" srcSet="" />
+            <span className="weather-description">
+              {" "}
+              {data.weather[0].description}
+            </span>
+          </div>
+          <div className="weatherdetails">
+            <div className="section1">
+              <table>
+                <tr>
+                  <td>
+                    <h4>High/Low</h4>
+                  </td>
+                  <td>
+                    <span>
+                      {Math.floor(data.main.temp_max - 273.15)}/
+                      {Math.floor(data.main.temp_min - 273.15)}
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4>Humidity</h4>
+                  </td>
+                  <td>
+                    <span>{data.main.humidity} %</span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4>Pressure</h4>
+                  </td>
+                  <td>
+                    <span>{data.main.pressure} hPa</span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4>Visibility</h4>
+                  </td>
+                  <td>
+                    <span>{data.visibility / 1000} Km</span>
+                  </td>
+                </tr>
+              </table>
+            </div>
+
+            <div className="section2">
+              <table>
+                <tr>
+                  <td>
+                    <h4>Wind</h4>
+                  </td>
+                  <td>
+                    <span>{Math.floor((data.wind.speed * 18) / 5)} km/hr</span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4>Wind Direction</h4>
+                  </td>
+                  <td>
+                    <span>
+                      {data.wind.deg}
+                      <sup>o</sup> deg
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4>Sunrise</h4>
+                  </td>
+                  <td>
+                    <span>
+                      {new Date(data.sys.sunrise * 1000).toLocaleTimeString()}
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    <h4>Sunset</h4>
+                  </td>
+                  <td>
+                    <span>
+                      {new Date(data.sys.sunset * 1000).toLocaleTimeString()}
+                    </span>
+                  </td>
+                </tr>
+              </table>
+            </div>
+
+
+          </div>
+          <div>
+                <span>
+                  {/* {(data2.list[0].dt_txt)}  {/*Date And Time
+                <ul>
+                  <li>{(data2.list[0].main.humidity)}</li>
+                  <li>{(data2.list[0].main.temp)}</li>
+                  <li>{(data2.list[0].main.temp_max)}</li>
+                  <li>{(data2.list[0].main.temp_min)}</li>
+                  <li>{(data2.list[0].main.pressure)}</li>
+                  <li>{(data2.list[0].main.sea_level)}</li>
+                </ul>
+                <h3>{(data2.list[0].weather[0].main)}</h3>
+                <h3>{(data2.list[0].weather[0].description)}</h3>    */}
+                </span>
+          </div>
+        </React.Fragment>
+      ) : (
+        <div className="maincard">
+          <h2>{data.message}</h2>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Display;
